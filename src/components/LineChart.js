2a375919df446f5490e164b485453f954149b29d
ikechukwu <ikechukwu@brandmobile.net>
2022-04-04 10:05:52 +0100

    this is a complete code for metacare test

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -11,7 +11,7 @@ class LineChart extends Component {
         const options = {
             theme: "light2",
             title: {
-                text: ""                
+                text: ""
             },
             axisY: {
                 title: "",
@@ -26,67 +26,78 @@ class LineChart extends Component {
                 color: this.props.chartColor,
                 type: "line",
                 xValueFormatString: "MMM",
-                dataPoints: dataPoints
-            }]
-        }
-        return (
-            <div className='margin-sm-end mb-3'>
-                <div className='card-border'>
-                    <div className='card-ro'>
-                        <div className='d-flex justify-content-between pb-3 break-point-row'>
-                            <div className='d-flex'>
-                                <h3 className='chart-top-text'>{this.props.title}</h3>
-                                <span className='chart-top-text-greenish-bg'>+4.14% </span>
+                dataPoints: dataPoints,
+                // [
+                //     { x: new Date(1483228800000), y: 12 },
+                //     { x: new Date(1485907200000), y: 19 },
+                //     { x: new Date(1488326400000), y: 48 },
+                //     { x: new Date(1491004800000), y: 19 },
+                //     { x: new Date(1493596800000), y: 40 },
+                //     { x: new Date(1496275200000), y: 30 },
+                //     { x: new Date(1498867200000), y: 35 },
+                //     { x: new Date(1501545600000), y: 45 },
+                // ]
+            }
+        ]
+    }
+    return(
+            <div className = 'margin-sm-end mb-3' >
+            <div className='card-border'>
+                <div className='card-ro'>
+                    <div className='d-flex justify-content-between pb-3 break-point-row'>
+                        <div className='d-flex'>
+                            <h3 className='chart-top-text'>{this.props.title}</h3>
+                            <span className='chart-top-text-greenish-bg'>+4.14% </span>
+                        </div>
+
+                        <div className='d-flex'>
+                            <div className='chart-top-text-card d-flex justify-content-between'>
+                                <span className='chart-top-text-danger' style={{ background: this.props.colors }}></span>
+                                <div>High Priority</div>
                             </div>
 
-                            <div className='d-flex'>
-                                <div className='chart-top-text-card d-flex justify-content-between'>
-                                    <span className='chart-top-text-danger' style={{ background: this.props.colors}}></span>
-                                    <div>High Priority</div>
-                                </div>
-
-                                <div className='chart-top-text-card d-flex justify-content-between'>
-                                    <div className=''>This Month</div> <i className='far fa-calendar'></i>
-                                </div>
+                            <div className='chart-top-text-card d-flex justify-content-between'>
+                                <div className=''>This Month</div> <i className='far fa-calendar'></i>
                             </div>
                         </div>
-                        <CanvasJSChart options={options}
-                            onRef={ref => this.chart = ref}
-                        />
-                        {/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
                     </div>
-                    <div className='vertical-line card-space onhide-sm-730'>
-                        <div className='chart-card'>
-                            <span className='avg-res'>{this.props.avgTime}</span>
-                            <h3 className='avg-time'>30 Mins</h3>
-                        </div>
+                    <CanvasJSChart options={options}
+                        onRef={ref => this.chart = ref}
+                    />
+                    {/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+                </div>
+                <div className='vertical-line card-space onhide-sm-730'>
+                    <div className='chart-card'>
+                        <span className='avg-res'>{this.props.avgTime}</span>
+                        <h3 className='avg-time'>30 Mins</h3>
+                    </div>
 
-                        <div className='chart-card'>
-                            <span className='avg-res'>{this.props.resTime}</span>
-                            <h3 className='avg-time'>1 Hour 30 Mins</h3>
-                        </div>
+                    <div className='chart-card'>
+                        <span className='avg-res'>{this.props.resTime}</span>
+                        <h3 className='avg-time'>1 Hour 30 Mins</h3>
                     </div>
                 </div>
+            </div>
             </div>
         );
     }
 
-    componentDidMount() {
-        var chart = this.chart;
-        fetch('data.json')
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                for (var i = 0; i < data.length; i++) {
-                    dataPoints.push({
-                        x: new Date(data[i].x),
-                        y: data[i].y
-                    });
-                }
-                chart.render();
-            });
-    }
+componentDidMount() {
+    var chart = this.chart;
+    fetch('data.json')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for (var i = 0; i < data.length; i++) {
+                dataPoints.push({
+                    x: new Date(data[i].x),
+                    y: data[i].y
+                });
+            }
+            chart.render();
+        });
+}
 }
 
 export default LineChart;                           
